c04fe29b6e7b5db4fa9c234d850b66095d71c0f5
Nikhilbej <71884094+Nikhilbej@users.noreply.github.com>
2020-09-27 13:26:35 +0200

    Add files via upload

--- LED_Toggling.py
+++ LED_Toggling.py
@@ -3,27 +3,32 @@ import RPi.GPIO as GPIO
 import time
 #Declaring our main function
 def main():
+    #Turning off warnings
     GPIO.setwarnings(False)
+    #Setting the mode
     GPIO.setmode(GPIO.BOARD)
-
+    #Choosing the pin for the LED
     ledPin = 11
-    GPIO.setmode(GPIO.BOARD)
+    #Setting up the pin
     GPIO.setup(ledPin, GPIO.OUT)
+    #Setting the LED pin high
     GPIO.output(ledPin, GPIO.HIGH)
     #delay for 5 seconds to notice the LED toggling
     time.sleep(5)
     #Setting the LED low
     GPIO.output(ledPin, GPIO.LOW)
-    #Cleanup any unused resources
-    GPIO.cleanup()
+
 
 
         
 #only run functions if this module is run
 if __name__ == "__main__":
     try:
-        main()
+        while True:
+            main()
     except KeyboardInterrupt:
         print("Exiting gracefully")
     except Exception as e:
         print("Error: {}".format(e))
+        #Cleanup any unused resources
+        GPIO.cleanup()
